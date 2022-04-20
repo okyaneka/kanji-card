024adc5d7cda5f1b8f552ce7249f393808a04b0a
Oky Aneka <okyaneka@gmail.com>
2022-04-20 09:58:06 +0700

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import { computed } from "@vue/reactivity";
+import { reactive, ref } from "vue";
+
+const counter = reactive({
+  count: 1,
+});
+const label = ref(null);
+
+const textCounter = computed(() => {
+  return `${label.value || ""}: ${counter.count}`;
+});
+
+function handleOnInput(event) {
+  label.value = event.target.value;
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome :label="textCounter" @change:text="handleOnInput" />
+  </main>
+</template>
